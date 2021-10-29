314e12065bb0aeaf35163993494ec4abc0c9c266
Rafael Romano <rafa-kun@hotmail.com>
2021-10-29 19:54:00 -0300

    :bug: Corrigindo requisito 6
    TOmara

--- desafio6.sql
+++ desafio6.sql
@@ -3,10 +3,11 @@ CREATE VIEW faturamento_atual AS
         CONCAT(MIN(valor), '.00') AS `faturamento_minimo`,
         CONCAT(MAX(valor)) AS `faturamento_maximo`,
         CONCAT(ROUND(AVG(valor), 1), '0') AS `faturamento_medio`,
-        CONCAT(SUM(valor)) AS `faturamento_total`
+        CONCAT((SELECT valor FROM SpotifyClone.Plano WHERE plano_id = 2)+(SELECT valor FROM SpotifyClone.Plano WHERE plano_id = 3)) AS `faturamento_total`
     FROM
         SpotifyClone.Plano AS P
             INNER JOIN
         SpotifyClone.Usuario AS U ON P.plano_id = U.plano_id;
+
 
 
--- desafio9.sql
+++ desafio9.sql
@@ -1,6 +1,8 @@
 DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(IN nomeArtista VARCHAR(50))
 BEGIN
-SELECT nome_artista AS `artista`, nome_album AS `album` FROM SpotifyClone.Artista AS Ar INNER JOIN SpotifyClone.Album AS Al ON Ar.artista_id = Al.artista_id WHERE Ar.nome_artista = nomeArtista;
+SELECT nome_artista AS `artista`, nome_album AS `album` FROM SpotifyClone.Artista 
+AS Ar INNER JOIN SpotifyClone.Album AS Al ON Ar.artista_id = Al.artista_id
+WHERE Ar.nome_artista = nomeArtista;
 END $$
 DELIMITER ;
